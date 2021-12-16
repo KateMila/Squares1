376116bda28e83733e650a05c2118f5cf6d8f8b7
KateMila <89808903+KateMila@users.noreply.github.com>
2021-12-16 21:07:04 +0300

    Fifth commit

--- src/main/java/ru/netology/sqr/Main.java
+++ src/main/java/ru/netology/sqr/Main.java
@@ -9,6 +9,6 @@ public class Main {
         SQRService service = new SQRService();
         service.squares(numberFirst, numberTwo);
 
-        return;
     }
+
 }
--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,8 +4,10 @@ public class SQRService {
 
     public int squares(int numberFirst, int numberTwo) {
         int result = 0;
-        for (int i = 10; i <= 99; i++) {
-            if (i * i >= numberFirst && i * i <= numberTwo) {
+        for (int i = 10; i <= 99; i++)
+        {
+            if (i * i >= numberFirst && i * i <= numberTwo)
+            {
                 result++;
             }
         }
